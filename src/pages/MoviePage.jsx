41613852b323b96312d2578a09aef989ba05b167
andrealangianese <andre.langia.01@gmail.com>
2026-02-23 15:54:09 +0100

    creo page sempre statica del dettaglio dei film

---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,18 @@
+const MoviePage = () => {
+    return (
+        <>
+            <h1 className="text-primary">pagina dei film  </h1>
+            <h2><i>Sarà comunque tutto testo che andrò a sostituire 'dinamicamente'</i></h2>
+
+            <div>
+                <p className="row row-cols-3 mt-4">
+                    titolo film
+                    vari dettagli
+                    e recensioni
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default MoviePage
