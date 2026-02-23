bbbbe92cb8cb2eabadd5016fe3fda3d80755ff7a
andrealangianese <andre.langia.01@gmail.com>
2026-02-23 15:17:00 +0100

    creo cartella components con header all'interno per 'ritornare indietro'

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function MainHeader(){
+    
+    return(
+        <nav>
+            <Link to='/'>return back</Link>
+        </nav>
+    )
+}
