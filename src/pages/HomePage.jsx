97a584ac0e2c0b5e2804e3ccf2fd920c13df08d9
andrealangianese <andre.langia.01@gmail.com>
2026-02-23 15:49:28 +0100

    creo cartella pages con HomePage(per ora statico)

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+const HomePage = () => {
+    return (
+        <>
+            <h1 className="text-primary">And Movies </h1>
+            <h2><i>Sarà comunque tutto testo che andrò a sostituire 'dinamicamente'</i></h2>
+
+            <div>
+                <p className="row row-cols-3 mt-4">
+                    qui ci sarà l'elenco dei movie
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
