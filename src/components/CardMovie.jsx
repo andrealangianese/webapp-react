806afa90292f710ffa667df8d81cdeca36ec194c
andrealangianese <andre.langia.01@gmail.com>
2026-02-23 17:09:46 +0100

    creo componente cardmovie statica

---
+++ src/components/CardMovie.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom"
+
+const CardMovie = () => {
+    return (
+        <div className="card mb-4">
+            <img 
+                src="https://www.instagram.com/p/DRfiffxDCfP/" 
+                className="card-img-top" 
+                alt="Titolo del film" 
+            />
+            
+            <div className="card-body">
+                <h5 className="card-title">Titolo del Film</h5>
+                
+                <p className="mb-1">
+                    <strong>Regista:</strong> Nome Regista
+                </p>
+                
+                <p className="mb-1">
+                    <strong>Genere:</strong> Azione / Drammatico
+                </p>
+                
+                <p className="mb-2">
+                    <strong>Anno:</strong> 2024
+                </p>
+
+                <p className="card-text">
+                    Questo paragrafo verrà sostituito con l'abstract del film.
+                </p>
+
+                <Link to={`movies/1`} className="btn btn-primary">
+                    See more
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default CardMovie
