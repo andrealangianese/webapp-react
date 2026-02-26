da9014d89a5f15ac3b723399da0713e8a20e0ef6
andrealangianese <andre.langia.01@gmail.com>
2026-02-26 13:44:53 +0100

    creo nuovo components 'loader'

---
+++ src/components/Loader.jsx
@@ -0,0 +1,6 @@
+export default function Loader() {
+    return (
+        <div className="spinner-border text-success" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </div>
+        )}
