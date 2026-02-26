bc2948fabe2783e3d28305fdde93d3d6c1ed65b8
andrealangianese <andre.langia.01@gmail.com>
2026-02-26 14:12:26 +0100

    creo hook e provider per gestire il context che ho creato

--- context/GlobalContext.jsx
+++ context/GlobalContext.jsx
@@ -5,3 +5,25 @@ import { createContext, useContext, useState } from "react";
 
 const GlobalContext = createContext();
 
+// creo provider per il contesto globale
+
+export const GlobalProvider = ({ children }) => {
+
+    // imposto var di stato per gestire i dati globali
+    
+    const [loading, setLoading] = useState(false);
+
+    return (
+        <GlobalContext.Provider value={{ loading, setLoading }}>
+            {/* children conterrà tutta l'app */}
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+// creo hook per utilizzare il contesto globale
+
+export const useGlobalContext = () => {
+    // richiamo useContext per accedere al contesto globale e gli passo il contesto che ho creato
+    return useContext(GlobalContext);
+}
