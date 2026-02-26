65864b5890172551834651935cb535b4a913e7b5
andrealangianese <andre.langia.01@gmail.com>
2026-02-26 13:59:05 +0100

    importo il loader per visualizzarlo in pagina (defaullayout)

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,16 +1,22 @@
 import { Outlet } from "react-router-dom"
 
 import MainHeader from "../components/MainHeader"
+import Loader from "../components/Loader"
 
 function DefaultLayout() {
 
+    // creo var temporanea per simulare caricamento dati
+
+    const loading = true;
     return (
         <>
             <MainHeader />
+            {loading && <Loader />}
             <main className="container">
                 <Outlet />
             </main >
             <footer>ex di footer fisso</footer>
+            
         </>
     )
 }
