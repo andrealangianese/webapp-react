89d744157ccddc687c795ab98567e5dd2e8fa007
andrealangianese <andre.langia.01@gmail.com>
2026-02-26 14:06:25 +0100

    importo i componenti da react per creare il context e creo context globale da inserire poi in app

---
+++ context/GlobalContext.jsx
@@ -0,0 +1,7 @@
+// importo i componenti di React per composizione context
+import { createContext, useContext, useState } from "react";
+
+// creo contesto globale
+
+const GlobalContext = createContext();
+
